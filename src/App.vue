2624b5f4dcd3c3a5806cce952faa39dc20c7fb79
PauloSilas99 <fonsecasodreps@hotmail.com>
2024-08-02 10:57:31 -0300

    estrutura principal

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
+  <Footer />
 </template>
+<script>
+import Footer from './components/Footer.vue';
+import Navbar from './components/Navbar.vue';
 
+  export default {
+    components:{
+      Navbar,
+      Footer
+    },
+    data(){
+      return {
+        logo_src:"./img/logo.png"
+      }
+    }
+  }
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  *{
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .main-conatiner{
+    margin: 50px;
+    min-height: 250px;
+  }
+  h1{
+    text-align: center;
+    font-size: 42px;
+    color: #222;
+    margin-bottom: 30px;
+  }
 </style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-conatiner">
+    <h1>Gerenciar Pedidos:</h1>
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from '@/components/Dashboard.vue';
+
+  export default {
+    name:"Pedidos",
+    components:{
+      Dashboard
+    }
+  }
+</script>
